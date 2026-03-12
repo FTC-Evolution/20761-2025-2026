797b3940f29c783625af42df92ac0a8c4476a04b
Ilyas <uncognic@1707070.xyz>
2026-03-11 22:25:29 -0400

    apriltag (but we won't need it)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AutonomousMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AutonomousMode.java
@@ -6,11 +6,14 @@ import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.IMU;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.navigation.Position;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
@@ -20,6 +23,11 @@ import org.firstinspires.ftc.teamcode.Subsystems.Intake;
 import org.firstinspires.ftc.teamcode.Subsystems.IntakeThing;
 import org.firstinspires.ftc.teamcode.Subsystems.Shooter;
 import org.firstinspires.ftc.teamcode.Subsystems.Led;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+import android.util.Size;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import java.util.List;
 
 import java.util.Locale;
 
@@ -28,7 +36,13 @@ import java.util.Locale;
 
 
 public class AutonomousMode extends LinearOpMode {
-
+    private static final boolean USE_WEBCAM = true;
+    private final Position cameraPosition = new Position(DistanceUnit.INCH,
+            0, 0, 0, 0);
+    private final YawPitchRollAngles cameraOrientation = new YawPitchRollAngles(AngleUnit.DEGREES,
+            0, -90, 0, 0);
+    AprilTagProcessor aprilTag;
+    VisionPortal visionPortal;
     DrivetrainFO drivetrain;
     IMU imu;
     Intake intake;
@@ -48,6 +62,7 @@ public class AutonomousMode extends LinearOpMode {
     double headingStartingPosition = 0.0;
     @Override
     public void runOpMode() {
+        initAprilTag();
         imu = hardwareMap.get(IMU.class, "imu");
         IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
                 RevHubOrientationOnRobot.LogoFacingDirection.LEFT,
@@ -109,6 +124,7 @@ public class AutonomousMode extends LinearOpMode {
         waitForStart();
 
         if (opModeIsActive()) {
+            telemetryAprilTag();
             telemetry.addData("Status", "Running");
             telemetry.update();
 
@@ -315,7 +331,52 @@ public class AutonomousMode extends LinearOpMode {
         String data2 = String.format(Locale.US, "{X: %.3f, Y: %.3f, H: %.3f}", targetPose.getX(DistanceUnit.INCH), targetPose.getY(DistanceUnit.INCH), targetPose.getHeading(AngleUnit.RADIANS));
         telemetry.addData("TARGET Position", data2);
     }
+    private void telemetryAprilTag() {
 
+        List<AprilTagDetection> currentDetections = aprilTag.getDetections();
+        telemetry.addData("# AprilTags Detected", currentDetections.size());
+
+        for (AprilTagDetection detection : currentDetections) {
+            if (detection.metadata != null) {
+                telemetry.addLine(String.format(Locale.ENGLISH, "\n==== (ID %d) %s", detection.id, detection.metadata.name));
+                if (!detection.metadata.name.contains("Obelisk")) {
+                    telemetry.addLine(String.format(Locale.ENGLISH, "XYZ %6.1f %6.1f %6.1f  (inch)",
+                            detection.robotPose.getPosition().x,
+                            detection.robotPose.getPosition().y,
+                            detection.robotPose.getPosition().z));
+                    telemetry.addLine(String.format(Locale.ENGLISH, "PRY %6.1f %6.1f %6.1f  (deg)",
+                            detection.robotPose.getOrientation().getPitch(AngleUnit.DEGREES),
+                            detection.robotPose.getOrientation().getRoll(AngleUnit.DEGREES),
+                            detection.robotPose.getOrientation().getYaw(AngleUnit.DEGREES)));
+                }
+            } else {
+                telemetry.addLine(String.format(Locale.ENGLISH, "\n==== (ID %d) Unknown", detection.id));
+                telemetry.addLine(String.format(Locale.ENGLISH, "Center %6.0f %6.0f   (pixels)", detection.center.x, detection.center.y));
+            }
+        }
+        telemetry.addLine("\nkey:\nXYZ = X (Right), Y (Forward), Z (Up) dist.");
+        telemetry.addLine("PRY = Pitch, Roll & Yaw (XYZ Rotation)");
+    }
+    private void initAprilTag() {
+        aprilTag = new AprilTagProcessor.Builder()
+                .setCameraPose(cameraPosition, cameraOrientation)
+                .build();
+        VisionPortal.Builder builder = new VisionPortal.Builder();
+
+        if (USE_WEBCAM) {
+            builder.setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"));
+        } else {
+            builder.setCamera(BuiltinCameraDirection.BACK);
+        }
+
+        builder.setCameraResolution(new Size(640, 480));
+
+        builder.setStreamFormat(VisionPortal.StreamFormat.MJPEG);
+
+        builder.addProcessor(aprilTag);
+
+        visionPortal = builder.build();
+    }
 
 }
 
